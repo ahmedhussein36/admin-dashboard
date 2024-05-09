fc961970d0e2dca27e8b26e461f4fb9a3c305df4
Ahmed Hussein <105352918+ahmedhussein36@users.noreply.github.com>
2024-05-09 17:16:37 +0300

    Update prismadb.ts

--- app/libs/prismadb.ts
+++ app/libs/prismadb.ts
@@ -1,12 +1,11 @@
-import { PrismaClient } from "@prisma/client"
-import { withAccelerate } from '@prisma/extension-accelerate'
-
+import { PrismaClient } from "@prisma/client";
+import { withAccelerate } from "@prisma/extension-accelerate";
 
 declare global {
-  var prisma: PrismaClient | undefined
+    var prisma: PrismaClient | undefined;
 }
 
-const client = new PrismaClient().$extends(withAccelerate())
-// if (process.env.NODE_ENV !== "production") globalThis.prisma = client
+const client = new PrismaClient();
+if (process.env.NODE_ENV !== "production") globalThis.prisma = client;
 
-export default client
+export default client;
