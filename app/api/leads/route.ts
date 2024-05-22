5a295d039af1bd21897d23d98cd118237a037e1d
Ahmed Hussein <105352918+ahmedhussein36@users.noreply.github.com>
2024-05-22 17:09:25 +0300

    Update route.ts

--- app/api/leads/route.ts
+++ app/api/leads/route.ts
@@ -2,6 +2,18 @@ import { NextResponse } from "next/server";
 import prisma from "@/app/libs/prismadb";
 
 export async function POST(request: Request) {
+    // Set CORS headers
+    const headers = new Headers({
+        "Access-Control-Allow-Origin": "http://127.0.0.1:5500/", // Adjust this to allow specific origins
+        "Access-Control-Allow-Methods": "POST, GET, OPTIONS",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    });
+
+    // Handle preflight requests
+    if (request.method === "OPTIONS") {
+        return new Response(null, { headers });
+    }
+
     const body = await request.json();
     const { email, name, phone, subject } = body;
 
@@ -14,5 +26,9 @@ export async function POST(request: Request) {
         },
     });
 
-    return NextResponse.json(user);
+    // Include CORS headers in the response
+    return new Response(JSON.stringify(user), {
+        headers: headers,
+        status: 200,
+    });
 }
